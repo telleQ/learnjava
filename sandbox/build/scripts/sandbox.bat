7c66339c923631542064ab508ce23c4f285a01e6
telleQ <step.nst@gmail.com>
2022-04-07 20:38:09 +0300

    created project for idea

--- sandbox/build/scripts/sandbox.bat
+++ sandbox/build/scripts/sandbox.bat
@@ -71,7 +71,7 @@ set CLASSPATH=%APP_HOME%\lib\sandbox.jar
 
 
 @rem Execute sandbox
-"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %SANDBOX_OPTS%  -classpath "%CLASSPATH%" simple %*
+"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %SANDBOX_OPTS%  -classpath "%CLASSPATH%" Simple %*
 
 :end
 @rem End local scope for the variables with windows NT shell
